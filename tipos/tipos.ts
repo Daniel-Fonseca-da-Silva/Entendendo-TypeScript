eb4f7a06a02119fb4c494ad7b7c1c490a371368f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-14 22:43:43 -0300

    Tipos numéricos e booleano

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,4 +1,15 @@
 // string
 let nome = "Nikolai";
 console.log(nome);
-nome = 28;
+// nome = 28;
+
+let idade = 28;
+// idade = "Daniel";
+
+// idade 28.5;
+
+console.log(idade);
+
+let habilidades = false;
+// habilidades = 1;
+console.log(habilidades);
