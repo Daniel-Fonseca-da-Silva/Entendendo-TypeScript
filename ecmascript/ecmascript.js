95e4741a2beddc8cdfda79938c3878403a6a2dd6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-24 18:49:23 -0300

    Let e Const #01

---
+++ ecmascript/ecmascript.js
@@ -0,0 +1,13 @@
+"use strict";
+// Let & Const
+let seraQuePode = "?"; // Hoisting
+console.log(seraQuePode);
+let estaFrio = true;
+if (estaFrio) {
+    let acao = "Colocar um casaco";
+    console.log(acao);
+}
+const cpf = "000.000.000-00";
+// cpf = "111.111.111-11";
+console.log(cpf);
+//# sourceMappingURL=ecmascript.js.map
---
+++ ecmascript/ecmascript.ts
@@ -0,0 +1,13 @@
+// Let & Const
+let seraQuePode = "?"; // Hoisting
+console.log(seraQuePode);
+
+let estaFrio = true;
+if (estaFrio) {
+  let acao = "Colocar um casaco";
+  console.log(acao);
+}
+
+const cpf: string = "000.000.000-00";
+// cpf = "111.111.111-11";
+console.log(cpf);
