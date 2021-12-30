c160dba6a3abaea2d3c39edc04ddfe2c5d77762f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-30 00:09:52 -0300

    Getters & Setters

--- classes/classes.js
+++ classes/classes.js
@@ -106,4 +106,23 @@ console.log(niva.acelerar());
 console.log(niva.acelerar());
 console.log(niva.acelerar());
 console.log(niva.frear());
+// Getters & setters
+class Pessoa {
+    constructor() {
+        this.idade = 0;
+        // get idade(): number {
+        //   return this._idade;
+        // }
+        // set idade(valor: number) {
+        //   if (valor >= 0 && valor <= 120) {
+        //     this._idade = valor;
+        //   }
+        // }
+    }
+}
+const pessoa1 = new Pessoa();
+pessoa1.idade = 10;
+console.log(pessoa1);
+pessoa1.idade - 3;
+console.log(pessoa1.idade);
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -135,3 +135,25 @@ console.log(niva.acelerar());
 console.log(niva.acelerar());
 console.log(niva.acelerar());
 console.log(niva.frear());
+
+// Getters & setters
+class Pessoa {
+  idade: number = 0;
+
+  // get idade(): number {
+  //   return this._idade;
+  // }
+
+  // set idade(valor: number) {
+  //   if (valor >= 0 && valor <= 120) {
+  //     this._idade = valor;
+  //   }
+  // }
+}
+
+const pessoa1 = new Pessoa();
+pessoa1.idade = 10;
+console.log(pessoa1);
+
+pessoa1.idade - 3;
+console.log(pessoa1.idade);
