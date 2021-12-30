b3ea41ad505c96901783616baebd11d925198299
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-12-29 21:38:41 -0300

    Modificadores de Acesso

--- classes/classes.js
+++ classes/classes.js
@@ -53,4 +53,40 @@ console.log(monitor2.resumo());
 // Criar um método precoComDesconto
 // Quais são os parâmetros e o retorno?
 // Alterar método resumo para mostrar o preço com desconto
+class Carro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeAtual : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro("Dodge", "Le Baron", 170);
+Array(50)
+    .fill(0)
+    .forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+console.log(carro1.frear());
+console.log(carro1.frear());
+Array(100)
+    .fill(0)
+    .forEach(() => carro1.frear());
+console.log(carro1.frear());
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -70,3 +70,47 @@ console.log(monitor2.resumo());
 // Criar um método precoComDesconto
 // Quais são os parâmetros e o retorno?
 // Alterar método resumo para mostrar o preço com desconto
+
+class Carro {
+  private velocidadeAtual: number = 0;
+  constructor(
+    public marca: string,
+    public modelo: string,
+    private velocidadeMaxima: number = 200
+  ) {}
+
+  private alterarVelocidade(delta: number): number {
+    const novaVelocidade = this.velocidadeAtual + delta;
+    const velocidadeValida =
+      novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+
+    if (velocidadeValida) {
+      this.velocidadeAtual = novaVelocidade;
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeAtual : 0;
+    }
+
+    return this.velocidadeAtual;
+  }
+
+  public acelerar(): number {
+    return this.alterarVelocidade(5);
+  }
+
+  public frear(): number {
+    return this.alterarVelocidade(-5);
+  }
+}
+
+const carro1 = new Carro("Dodge", "Le Baron", 170);
+Array(50)
+  .fill(0)
+  .forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+
+console.log(carro1.frear());
+console.log(carro1.frear());
+Array(100)
+  .fill(0)
+  .forEach(() => carro1.frear());
+console.log(carro1.frear());
