428b9f7877084ae114358f66c9c8362ea2df1b45
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-02 18:23:35 -0300

    Carregando Módulos

--- modulos/modulos.js
+++ modulos/modulos.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const retangulo_1 = require("./retangulo");
 const circunferencia_1 = require("./circunferencia");
+console.log("Modulos carregados com sucesso!");
 console.log((0, retangulo_1.areaRetangulo)(7, 8));
 console.log((0, circunferencia_1.areaCircunferencia)(16));
 //# sourceMappingURL=modulos.js.map
--- modulos/modulos.ts
+++ modulos/modulos.ts
@@ -1,5 +1,6 @@
 import { areaRetangulo } from "./retangulo";
 import { areaCircunferencia } from "./circunferencia";
 
+console.log("Modulos carregados com sucesso!");
 console.log(areaRetangulo(7, 8));
 console.log(areaCircunferencia(16));
