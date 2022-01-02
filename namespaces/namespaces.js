a853457c89503e32280e60c492e223d69734cabf
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-02 17:48:03 -0300

    Módulos

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,6 +1,6 @@
 "use strict";
-const PI = 1.99;
+// const PI = 1.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
-console.log(PI);
+// console.log(PI);
 //# sourceMappingURL=namespaces.js.map
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,4 +1,4 @@
-const PI = 1.99;
+// const PI = 1.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
-console.log(PI);
+// console.log(PI);
