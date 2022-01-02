f56d71ee297cab2c2c9d4aba140471b89dd49b99
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-02 16:33:08 -0300

    Namespaces Imports

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,25 +1,8 @@
-var Geometria;
-(function (Geometria) {
-    var Area;
-    (function (Area) {
-        var PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
-var Geometria;
-(function (Geometria) {
-    var Area;
-    (function (Area) {
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
-var PI = 1.99;
+"use strict";
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRect.ts"/>
+const PI = 1.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
 console.log(PI);
+//# sourceMappingURL=namespaces.js.map
