e8a274e60aeb5ce820cb611e89c6c8db72afe9c9
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-02 14:39:58 -0300

    Namespaces em Múltiplos Arquivos

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,21 +1,25 @@
-"use strict";
 var Geometria;
 (function (Geometria) {
-    let Area;
+    var Area;
     (function (Area) {
-        const PI = 3.14;
+        var PI = 3.14;
         function circunferencia(raio) {
             return PI * Math.pow(raio, 2);
         }
         Area.circunferencia = circunferencia;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
+var Geometria;
+(function (Geometria) {
+    var Area;
+    (function (Area) {
         function retangulo(base, altura) {
             return base * altura;
         }
         Area.retangulo = retangulo;
     })(Area = Geometria.Area || (Geometria.Area = {}));
 })(Geometria || (Geometria = {}));
-const PI = 1.99;
+var PI = 1.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
 console.log(PI);
-//# sourceMappingURL=namespaces.js.map
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,17 +1,3 @@
-namespace Geometria {
-  export namespace Area {
-    const PI = 3.14;
-
-    export function circunferencia(raio: number): number {
-      return PI * Math.pow(raio, 2);
-    }
-
-    export function retangulo(base: number, altura: number): number {
-      return base * altura;
-    }
-  }
-}
-
 const PI = 1.99;
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
