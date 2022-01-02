d60858649515466084915db9bd02274b1a640c92
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-02 14:22:07 -0300

    Namespaces Aninhados

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -1,18 +1,21 @@
 "use strict";
-var Areas;
-(function (Areas) {
-    const PI = 3.14;
-    function circunferencia(raio) {
-        return PI * Math.pow(raio, 2);
-    }
-    Areas.circunferencia = circunferencia;
-    function retangulo(base, altura) {
-        return base * altura;
-    }
-    Areas.retangulo = retangulo;
-})(Areas || (Areas = {}));
+var Geometria;
+(function (Geometria) {
+    let Area;
+    (function (Area) {
+        const PI = 3.14;
+        function circunferencia(raio) {
+            return PI * Math.pow(raio, 2);
+        }
+        Area.circunferencia = circunferencia;
+        function retangulo(base, altura) {
+            return base * altura;
+        }
+        Area.retangulo = retangulo;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
 const PI = 1.99;
-console.log(Areas.circunferencia(10));
-console.log(Areas.retangulo(10, 20));
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
 console.log(PI);
 //# sourceMappingURL=namespaces.js.map
--- namespaces/namespaces.ts
+++ namespaces/namespaces.ts
@@ -1,16 +1,18 @@
-namespace Areas {
-  const PI = 3.14;
+namespace Geometria {
+  export namespace Area {
+    const PI = 3.14;
 
-  export function circunferencia(raio: number): number {
-    return PI * Math.pow(raio, 2);
-  }
+    export function circunferencia(raio: number): number {
+      return PI * Math.pow(raio, 2);
+    }
 
-  export function retangulo(base: number, altura: number): number {
-    return base * altura;
+    export function retangulo(base: number, altura: number): number {
+      return base * altura;
+    }
   }
 }
 
 const PI = 1.99;
-console.log(Areas.circunferencia(10));
-console.log(Areas.retangulo(10, 20));
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
 console.log(PI);
