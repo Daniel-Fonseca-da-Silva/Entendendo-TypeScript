513aa17097984457e32bc0221b127d5cdbb286bb
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-02 23:50:16 -0300

    Uso de Interface para estender Object

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -59,4 +59,23 @@ class AbstrataABD {
     a() { }
     b() { }
 }
+Object.prototype.log = function () {
+    console.log(this.toString());
+};
+const x = 2;
+const y = 3;
+const z = 4;
+console.log(x);
+console.log(y);
+console.log(z);
+x.log();
+y.log();
+z.log();
+const cli = {
+    name: "Erik",
+    toString() {
+        return this.name;
+    },
+};
+cli.log();
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -97,3 +97,31 @@ abstract class AbstrataABD implements A, B {
   b(): void {}
   abstract d(): void;
 }
+
+interface Object {
+  log(): void;
+}
+
+Object.prototype.log = function () {
+  console.log(this.toString());
+};
+
+const x = 2;
+const y = 3;
+const z = 4;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+x.log();
+y.log();
+z.log();
+
+const cli = {
+  name: "Erik",
+  toString() {
+    return this.name;
+  },
+};
+cli.log();
