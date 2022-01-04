d7d79de59d2200191fdebe2429570563d12e9bec
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-03 22:20:28 -0300

    Criando uma funçao com generics

--- generics/generics.js
+++ generics/generics.js
@@ -5,4 +5,11 @@ function echo(objeto) {
 console.log(echo("Nikolai").length);
 console.log(echo(27).length);
 console.log(echo({ nome: "Nikolai", idade: 20 }));
+// Usando generics
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado("Nikolai").length);
+console.log(echoMelhorado(27));
+console.log(echoMelhorado({ nome: "Nikolai", idade: 20 }));
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -5,3 +5,12 @@ function echo(objeto: any) {
 console.log(echo("Nikolai").length);
 console.log(echo(27).length);
 console.log(echo({ nome: "Nikolai", idade: 20 }));
+
+// Usando generics
+function echoMelhorado<T>(objeto: T): T {
+  return objeto;
+}
+
+console.log(echoMelhorado("Nikolai").length);
+console.log(echoMelhorado<number>(27));
+console.log(echoMelhorado({ nome: "Nikolai", idade: 20 }));
