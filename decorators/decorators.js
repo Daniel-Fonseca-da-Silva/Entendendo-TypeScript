6568502f2f010fb15caf006f244f88920eac2d62
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-06 21:03:33 -0300

    Desafio Decorator de Classe

--- decorators/decorators.js
+++ decorators/decorators.js
@@ -1,4 +1,10 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 function logarClasse(construtor) {
     console.log(construtor);
 }
@@ -41,4 +47,29 @@ const eletro = new Eletrodomestico();
 eletro.imprimir && eletro.imprimir();
 eletro.imprimir && eletro.imprimir();
 eletro.imprimir && eletro.imprimir();
+// Desafio Decorator perfilAdmin
+const usuarioLogado = {
+    nome: "Guilherme Filho",
+    email: "guigui@gmail.com",
+    admin: true,
+};
+let MudancaAdministrativa = class MudancaAdministrativa {
+    critico() {
+        console.log("Algo crítico foi alterado!");
+    }
+};
+MudancaAdministrativa = __decorate([
+    perfilAdmin
+], MudancaAdministrativa);
+function perfilAdmin(construtor) {
+    return class extends construtor {
+        constructor(...args) {
+            super(...args);
+            if (!usuarioLogado || !usuarioLogado.admin) {
+                throw new Error("Sem permissão");
+            }
+        }
+    };
+}
+new MudancaAdministrativa().critico();
 //# sourceMappingURL=decorators.js.map
--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -61,3 +61,30 @@ const eletro = new Eletrodomestico();
 eletro.imprimir && eletro.imprimir();
 eletro.imprimir && eletro.imprimir();
 eletro.imprimir && eletro.imprimir();
+
+// Desafio Decorator perfilAdmin
+const usuarioLogado = {
+  nome: "Guilherme Filho",
+  email: "guigui@gmail.com",
+  admin: true,
+};
+
+@perfilAdmin
+class MudancaAdministrativa {
+  critico() {
+    console.log("Algo crítico foi alterado!");
+  }
+}
+
+function perfilAdmin<T extends Construtor>(construtor: T) {
+  return class extends construtor {
+    constructor(...args: any[]) {
+      super(...args);
+      if (!usuarioLogado || !usuarioLogado.admin) {
+        throw new Error("Sem permissão");
+      }
+    }
+  };
+}
+
+new MudancaAdministrativa().critico();
